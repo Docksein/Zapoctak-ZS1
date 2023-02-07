1c5efac9b001676c0699292ba7ae923bc3a42dd4
Adam <xdocad01@gjk.cz>
2023-02-07 23:58:06 +0100

    docstringy

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ class Converter():
 
 # Třída na konvertování elementů markdownu do html bez změny konverterem 
 class SyntaxElements(Converter):
+	"""
+	Převede elementy markdownu na html kódy, které zobrazují daný znak
+	"""
 	regex = re.compile(r"(\*)|(\`)|(#)|(>)|(<)|(---)")
 
 	def replace(self, match):
@@ -35,6 +38,9 @@ class SyntaxElements(Converter):
 			return f"&#45;&#45;&#45;"
 # Třída na ignorování konvertování markdown elementů
 class Escapers(Converter):
+	"""
+	převádí zalomené znaky na html symboly
+	"""
 	regex = re.compile(r"(\\\*)|(\\\`)")
 
 	def replace(self, match):
@@ -44,6 +50,9 @@ class Escapers(Converter):
 			return f"&#96;"
 #Třída na konvertování nadpisů
 class HeadingConverter(Converter):
+	"""
+	Konvertuje markdown nadpisy #(1-6krát) na html <h(1-6)>
+	"""
 	regex = re.compile(r"^\s*(#{1,6})(.*?)$", flags=re.MULTILINE)
 
 	def replace(self, match):
@@ -53,6 +62,9 @@ class HeadingConverter(Converter):
 
 # Třída na konvertování horizontálních čar
 class HorizontalRuleConverter(Converter):
+	"""
+	Konvertuje markdown horizontální čáry --- na html <hr>
+	"""
 	regex = re.compile(r"\*\*\*|---")
 
 	def replace(self, match):
@@ -60,12 +72,18 @@ class HorizontalRuleConverter(Converter):
 
 # Třídy na konvertování odkazů, obrázků
 class LinkConverter(Converter):
+	"""
+	Konvertuje markdown odkazy [title](https://www.example.com) na html <a href='https://www.example.com'>title</a>
+	"""
 	regex = re.compile(r"\[(.+?)\]\((.*?)\)", flags=re.MULTILINE)
 
 	def replace(self, match):
 		return f"<a href='{match.group(2)}'>{match.group(1)}</a>"
 
 class ImageConverter(Converter):
+	"""
+	Konvertuje markdown obrázků ![alt text](image.jpg "title(nepovinně)") na html <img src="image.jpg" alt="alt text" title="title")>
+	"""
 	regex = re.compile(r"\!\[(.+?)\]\((.*?)\)")
 
 	def replace(self, match):
@@ -77,6 +95,9 @@ class ImageConverter(Converter):
 			return f'<img src="{match_group2}" alt="{match.group(1)}">'
 #Třída na konvertování kódu
 class CodeConverter(Converter):
+	"""
+	Konvertuje markdown kód uvozovaný zpětnými uvozovkami `kód` na html <code>kód</code>
+	"""
 	regex = re.compile(r"`(.*?)`", flags=re.DOTALL)
 
 	def replace(self, match):
@@ -85,6 +106,10 @@ class CodeConverter(Converter):
 
 # Třídy na konvertování bloků textu nebo seznamu
 class BlockQuoteConverter(Converter):
+	"""
+	Konvertuje markdown blok citace, dokud začíná řádek znakem > na html <blockquote>
+	"""
+
 	regex = re.compile(r"^>(.*?)(^[^>]|\Z)", flags=re.MULTILINE | re.DOTALL)
 
 	def replace(self, match):
@@ -93,6 +118,9 @@ class BlockQuoteConverter(Converter):
 
 
 class ListItemsConverter(Converter):
+	"""
+	Konvertuje jednotlivé položky v seznamech na <li>položka</li>
+	"""
 	regex = re.compile(r"^\d*\.(.*?)$|-(\s*.*?)$", flags = re.MULTILINE)
 
 	def replace(self, match):
@@ -102,6 +130,9 @@ class ListItemsConverter(Converter):
 			return f"<li>{match.group(2)}</li>"
 
 class OrderedListConverter(Converter):
+	"""
+	Konvertuje očíslovaný seznam z markdownu na html <ul>
+	"""
 	regex = re.compile(r"^(\d*\..*?)(^[^\d]|\Z)", flags=re.MULTILINE | re.DOTALL)
 
 	def replace(self, match):
@@ -109,6 +140,9 @@ class OrderedListConverter(Converter):
 		return f"<ol>\n{match_group1}</ol>\n{match.group(2)}"
 
 class UnorderedListConverter(Converter):
+	"""
+	Konvertuje neočíslovaný seznam z markdownu na html <ol>
+	"""
 	regex = re.compile(r"^(-.*?)(^[^-]|\Z)", flags=re.MULTILINE | re.DOTALL)
 
 	def replace(self, match):
@@ -122,16 +156,20 @@ class InlineConverter(Converter):
 		return f"<{self.tag}>{match.group(1)}</{self.tag}>"
 
 class BoldConverter(InlineConverter):
+	"""
+	Konvertuje tučně napsané písmo z **text** na <strong>text</strong>
+	"""
 	regex = re.compile(r"\*\*(.*?)\*\*")
 	tag  = "strong"
 
 class ItalicConverter(InlineConverter):
+	"""
+	Konvertuje písmo napsané kurzívou z *text* na <em>text</em>
+	"""
 	regex = re.compile(r"\*(.*?)\*")
 	tag  = "em"
 
 txt = '''
-
-
 1. First item
 2. Second item
 3. Third item
@@ -144,7 +182,7 @@ txt = '''
 - Revenue was off the chart.
 - Profits were higher than ever.
 1. First item
-20 Second item
+20. Second item
 Third item
 
 
